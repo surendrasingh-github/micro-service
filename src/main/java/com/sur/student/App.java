b96343f7036f993f5b4fad98d0f5de49b47cb958
surendrasingh-github <ss>
2017-10-14 18:41:18 +0200

    First micro service

---
+++ src/main/java/com/sur/student/App.java
@@ -0,0 +1,14 @@
+package com.sur.student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    //public static void main( String[] args )
+    public  void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/sur/student/SampleController.java
@@ -0,0 +1,27 @@
+package com.sur.student;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World Surendra Singh!";
+    }
+    
+    @RequestMapping("/home")
+    @ResponseBody
+    String homeAddress() {
+        return "Avenue Henry Dunant, Evere, Brussels!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
